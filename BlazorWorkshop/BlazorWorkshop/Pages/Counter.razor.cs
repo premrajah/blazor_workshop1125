53b74591af0e33d93d8e7c4d17d40b595f59622f
Prem <prem.rajah@gmail.com>
2020-03-28 21:05:35 +0000

    Added CodeBehind to the Counter component for debugging

---
+++ BlazorWorkshop/BlazorWorkshop/Pages/Counter.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWorkshop.Pages
+{
+    public class CounterCode : ComponentBase
+    {
+        public int currentCount { get; set; }
+
+        public void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
