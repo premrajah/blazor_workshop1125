b73f25d881a46ef340a776212b1ac883da6bcdb4
Prem <prem.rajah@gmail.com>
2020-04-13 21:35:50 +0100

    attempted logging for signalR added code behind for TestComponent added code behind for Index (does not added it together ? )

---
+++ BlazorWorkshop/BlazorWorkshop/Pages/Index.cs
@@ -0,0 +1,28 @@
+﻿using BlazorWorkshop.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWorkshop.Pages
+{
+    public class IndexCode : ComponentBase
+    {
+        public string DisplayMessage = "";
+
+        public void CustomerSelected(Customer customer)
+        {
+            DisplayMessage = $"Event Raised. Customer Selected: {customer.Name}";
+        }
+
+        public List<Customer> Customers = new List<Customer>();
+
+        protected override void OnInitialized()
+        {
+            Customers.Add(new Customer { CustomerId = 1, Name = "Isadora Jarr" });
+            Customers.Add(new Customer { CustomerId = 2, Name = "Ben Slackin" });
+            Customers.Add(new Customer { CustomerId = 3, Name = "Doo Fuss" });
+        }
+    }
+}
---
+++ BlazorWorkshop/BlazorWorkshop/Pages/TestComponent.cs
@@ -0,0 +1,31 @@
+﻿using BlazorWorkshop.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWorkshop.Pages
+{
+    public class TestComponentCode : ComponentBase
+    {
+        public Customer SelectedCustomer;
+
+        [Parameter]
+        public List<Customer> Customers { get; set; } = new List<Customer>();
+
+        [Parameter]
+        public EventCallback<Customer> CustomerSelectEvent { get; set; }
+
+
+        public async Task CustomerSelected(ChangeEventArgs args)
+        {
+            SelectedCustomer = (from x in Customers
+                                where x.CustomerId.ToString() == args.Value.ToString()
+                                select x).First();
+
+            await CustomerSelectEvent.InvokeAsync(SelectedCustomer);
+
+        }
+    }
+}
